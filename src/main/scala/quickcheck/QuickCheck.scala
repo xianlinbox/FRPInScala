6736b8c54273f64c8b52169950130f42c4411b7d
Liu Xianning <xianlinbox@gmail.com>
2016-06-09 23:34:24 +0800

    add delteMin property check

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -9,15 +9,23 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  property("min1") = forAll { a: Int =>
+  property("findMin1") = forAll { a: Int =>
     val h = insert(a, empty)
     findMin(h) == a
   }
 
-  property("min2") = forAll { a: Int =>
+  property("findMin2") = forAll { a: Int =>
     val h1 = insert(a, empty)
     val h2 = insert(a + 1, h1)
     findMin(h2) == a
+  }
+
+
+  property("deleteMin1") = forAll { a: Int =>
+    val h1 = insert(a, empty)
+    val h2 = insert(a + 1, h1)
+    val h3 = deleteMin(h2)
+    findMin(h3) == a + 1
   }
 
 
