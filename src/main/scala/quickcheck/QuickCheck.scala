dea30ddc44086492f9f383e05efba2965602c404
Liu Xianning <xianlinbox@gmail.com>
2016-06-09 23:28:26 +0800

    add 2 new property check

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -14,6 +14,21 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
+  property("min2") = forAll { a: Int =>
+    val h1 = insert(a, empty)
+    val h2 = insert(a + 1, h1)
+    findMin(h2) == a
+  }
+
+
+  property("meld") = forAll { a: Int =>
+    val h1 = insert(a, empty)
+    val h2 = insert(a + 1, empty)
+    val h3 = meld(h1, h2)
+    findMin(h3) == a
+  }
+
+
   lazy val genHeap: Gen[H] = ???
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
